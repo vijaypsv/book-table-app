8a12a6fdb850984733fbe7ca651f91c7a3a57c5f
Vijay Sabnani <vijaypsv@gmail.com>
2024-02-01 10:08:22 +0100

    added hero section

--- src/Components/home/Hero.js
+++ src/Components/home/Hero.js
@@ -1,5 +1,25 @@
+import {Link} from "react-router-dom";
+import restaurantFood from '../../assets/restauranfood.jpg'
+import './Hero.css'
+
 const Hero = () => {
-  return <main>Hero</main>;
+  return <section className='hero'>
+  <div className='hero-info'>
+    <h1>Little Lemon</h1>
+    <h3>Chicago</h3>
+    <p className='description'>
+      We are a family owned Mediterranean restaurant, focused on traditional
+      recipes served with a modern twist.
+    </p>
+    <Link className="action-button" to="/reservations">Reserve a table</Link>
+  </div>
+  <div className='hero-image'>
+    <img
+      src={restaurantFood}
+      alt='Restaurant food'
+    />
+  </div>
+</section>;
 };
 
 export default Hero;
